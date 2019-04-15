4a146173549700ac8f2f8308b987b3a9868be539
867620516 <867620516@qq.com>
2019-04-15 17:28:26 +0800

    加了一个rental的get api接口

--- src/main/java/graduation/spring/erent/app/web/RentalController.java
+++ src/main/java/graduation/spring/erent/app/web/RentalController.java
@@ -3,6 +3,7 @@ package graduation.spring.erent.app.web;
 import graduation.spring.erent.app.model.Rental;
 import graduation.spring.erent.app.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,12 @@ public class RentalController {
         Rental r = list.get(0);
         String s = r.getItemName();
         return list;
+    }
+
+    @RequestMapping("/api/oneRental")
+    public Rental findOne(@PathVariable String id){
+        Rental rental = new Rental();
+        rental.setId(Integer.parseInt(id));
+        return  rentService.find(rental);
     }
 }
